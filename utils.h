86c1af1a8ef8d07f4bc989ea463858ae5b9f2adf
Ballbot <5252bb@daum.net>
2024-04-12 19:04:00 +0900

    가뭄에콩나듯안되는게 있음

--- utils.h
+++ utils.h
@@ -1,12 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define TETRIS_WIDTH 10
-#define TETRIS_HEIGHT 20
-#define TETRIS_UI_WIDTH 6
+#define TETRIS_UI_WIDTH 5
 #define TETRIS_UI_HEIGHT 6
 
 #include <string>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 #include "tetromino.h"
 
 const Tetromino DECLARED_TETROMINOS[] = {
@@ -21,6 +22,8 @@ const Tetromino DECLARED_TETROMINOS[] = {
 
 int getRandomTetrominoIndex();
 
+std::string timeFormat(clock_t);
+
 struct Tetromino2D {
   Tetromino* mino;
   int x;
@@ -32,6 +35,13 @@ enum CollisionType {
   OUT_OF_BOARD_Y = 1,
   CONFLICT_BLOCK = 2,
   NONE = 3,
+};
+
+enum GameStatus {
+  PLAYING = 0,
+  LOSE = 1,
+  WIN = 2,
+  EXIT = 3,
 };
 
 #endif
